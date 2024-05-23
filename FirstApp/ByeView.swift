907b0f504381625b976e35604a29c75eeb544b7b
sinaaskarnejad <sina.askarnejad@gmail.com>
2024-05-24 01:23:56 +0330

    Add a new view sample

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  FirstApp
+//
+//  Created by Sina Askarnejad on 12/15/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye, World!")
+    }
+}
+
+#Preview {
+    ByeView()
+}
